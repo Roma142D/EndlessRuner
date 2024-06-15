d7f7f1d022a275cee83d53adee95d0da5e607e09
Roman <romadoliba@gmail.com>
2024-06-15 21:59:47 +0300

    Add script to record best scores

---
+++ Assets/Scripts/UI/BestScoresScreenControler.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace RomanDoliba.UI
+{
+    public class BestScoresScreenControler : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI[] _scoresTables;
+        private int _lastScore;
+        private int _firstBestScore;
+        private int _secondBestScore;
+        private int _thirdBestScore;
+
+        private void Update()
+        {
+            _lastScore = PlayerPrefs.GetInt("LastScore");
+            
+            _firstBestScore = PlayerPrefs.GetInt("FirstBestScore");
+            _secondBestScore = PlayerPrefs.GetInt("SecondBestScore");
+            _thirdBestScore = PlayerPrefs.GetInt("ThirdBestScore");
+            
+            if (_lastScore > _firstBestScore)
+            {
+                PlayerPrefs.SetInt("FirstBestScore", _lastScore);
+            }
+            else if (_lastScore > _secondBestScore && _lastScore < _firstBestScore)
+            {
+                PlayerPrefs.SetInt("SecondBestScore", _lastScore);
+            }
+            else if (_lastScore > _thirdBestScore && _lastScore < _secondBestScore)
+            {
+                PlayerPrefs.SetInt("ThirdBestScore", _lastScore);
+            }
+            PlayerPrefs.Save();
+
+            AddScore(0, _firstBestScore);
+            AddScore(1, _secondBestScore);
+            AddScore(2, _thirdBestScore);
+        }
+
+        private void AddScore(int position, int score)
+        {
+            _scoresTables[position].SetText(score.ToString());
+        }
+    }
+}
--- Assets/Scripts/UI/GameScreenControler.cs
+++ Assets/Scripts/UI/GameScreenControler.cs
@@ -6,10 +6,14 @@ namespace RomanDoliba.UI
     public class GameScreenControler : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _scoreCounterText;
-        private int _curentScore = 0;
+        private int _curentScore;
         private float _changeableScore;
         private int _scoreMultiplier = 1;
 
+        private void Start()
+        {
+            _curentScore = PlayerPrefs.GetInt("LastScore", 0);
+        }
         private void FixedUpdate()
         {
             _scoreCounterText.SetText(_curentScore.ToString());
@@ -22,11 +26,16 @@ namespace RomanDoliba.UI
             _changeableScore += (1 * _scoreMultiplier) * 0.01f;
             _curentScore = (int)_changeableScore;
 
+            PlayerPrefs.SetInt("LastScore", _curentScore);
+            PlayerPrefs.Save();
+
             if (_curentScore % 1000 == 0)
             {
                 _scoreMultiplier += 1;
             }
         }
+
+        //public int CurrentScore{get{return _curentScore;}}
 
     }
 }
