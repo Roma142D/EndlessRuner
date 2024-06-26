7255fc1d989152b11d579eea995eb16eb570a755
Roman <romadoliba@gmail.com>
2024-06-26 12:59:40 +0300

    Made coins and diamonds colection
    To do: Add treasure spawner or pull obj
    Try to make scriptable obj for currency

--- Assets/Scripts/UI/GameScreenControler.cs
+++ Assets/Scripts/UI/GameScreenControler.cs
@@ -1,3 +1,4 @@
+using RomanDoliba.ActionSystem;
 using TMPro;
 using UnityEngine;
 
@@ -5,14 +6,26 @@ namespace RomanDoliba.UI
 {
     public class GameScreenControler : MonoBehaviour
     {
+        [Header("Score Counter")]
         [SerializeField] private TextMeshProUGUI _scoreCounterText;
         private int _curentScore;
         private float _changeableScore;
         private int _scoreMultiplier = 1;
+        [Space]
+        [Header("Coins Counter")]
+        [SerializeField] private TextMeshProUGUI _coinsCounterText;
+        [SerializeField] private string _coinsCollectEventName;
+        [SerializeField] private string _diamondsCollectEventName;
+        [SerializeField] private int _coinsAmountPerEvent;
+        private int _curentCoinsValue;
 
-        private void Start()
+        private void Awake()
         {
             _curentScore = PlayerPrefs.GetInt("LastScore", 0);
+            _curentCoinsValue = PlayerPrefs.GetInt("CoinsCount", 0);
+
+            _coinsCounterText.SetText($"Coins: {_curentCoinsValue}");
+            GlobalEventSender.OnEvent += OnColectEvent;
         }
         private void FixedUpdate()
         {
@@ -35,7 +48,25 @@ namespace RomanDoliba.UI
             }
         }
 
-        //public int CurrentScore{get{return _curentScore;}}
+        private void OnColectEvent(string eventName)
+        {
+            if (eventName == _coinsCollectEventName)
+            {
+                _curentCoinsValue += _coinsAmountPerEvent;
+                PlayerPrefs.SetInt("CoinsCount", _curentCoinsValue);
+                PlayerPrefs.Save();
+
+                _coinsCounterText.SetText($"Coins: {_curentCoinsValue}");
+            }
+            else if (eventName == _diamondsCollectEventName)
+            {
+                _curentCoinsValue += _coinsAmountPerEvent * 10;
+                PlayerPrefs.SetInt("CoinsCount", _curentCoinsValue);
+                PlayerPrefs.Save();
+
+                _coinsCounterText.SetText($"Coins: {_curentCoinsValue}");
+            }
+        }
 
     }
 }
